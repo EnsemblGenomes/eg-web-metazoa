26b18ee2c5eca3c30da8623d2ddb676184b1258c
Manoj <manojpandian@ebi.ac.uk>
2022-03-17 15:29:02 +0000

    Updated species for 107 (eg54)

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -28,140 +28,176 @@ sub update_conf {
   $SiteDefs::ENSEMBL_HMMER_ENABLED     = 1;
 
   $SiteDefs::PRODUCTION_NAMES = [sort qw(
-    actinia_equina_gca011057435
-    acyrthosiphon_pisum
-    adineta_vaga
-    aedes_aegypti_lvpagwg
-    aedes_albopictus
-    amphimedon_queenslandica
-    anopheles_albimanus
-    anopheles_arabiensis
-    anopheles_atroparvus
+    actinia_equina_gca011057435   
+    acyrthosiphon_pisum   
+    adineta_vaga   
+    aedes_aegypti_lvpagwg  
+    aedes_albopictus   
+    agrilus_planipennis_gca000699045v2  
+    amphimedon_queenslandica   
+    anneissia_japonica_gca011630105v1   
+    anopheles_albimanus  
+    anopheles_arabiensis   
+    anopheles_atroparvus  
+    anopheles_atroparvus_gca914969975
     anopheles_christyi
     anopheles_coluzzii
     anopheles_coluzzii_ngousso
     anopheles_culicifacies
     anopheles_darlingi
-    anopheles_dirus
-    anopheles_epiroticus
+    anopheles_dirus   
+    anopheles_epiroticus  
     anopheles_farauti
     anopheles_funestus
-    anopheles_gambiae
-    anopheles_maculatus
-    anopheles_melas
+    anopheles_gambiae   
+    anopheles_maculatus   
+    anopheles_melas  
     anopheles_merus
-    anopheles_minimus
-    anopheles_quadriannulatus
-    anopheles_sinensis
-    anopheles_sinensis_china
-    anopheles_stephensi
-    anopheles_stephensi_indian
-    anoplophora_glabripennis
+    anopheles_minimus   
+    anopheles_quadriannulatus   
+    anopheles_sinensis   
+    anopheles_sinensis_china   
+    anopheles_stephensi  
+    anopheles_stephensi_indian   
+    anoplophora_glabripennis  
+    aphidius_gifuensis_gca014905175v1
     apis_mellifera
+    aplysia_californica_gca000002075v2
     ascaris_suum
-    atta_cephalotes
+    atta_cephalotes   
+    bactrocera_dorsalis_gca000789215v2  
+    bactrocera_latifrons_gca001853355v1
+    bactrocera_tryoni_gca016617805v2
     belgica_antarctica
     bemisia_tabaci_asiaii5
     bemisia_tabaci_ssa1nig
-    bemisia_tabaci_ssa1ug
-    bemisia_tabaci_ssa2nig
-    bemisia_tabaci_ssa3nig
-    bemisia_tabaci_sweetpotug
-    biomphalaria_glabrata
-    bombus_impatiens
-    bombus_terrestris
-    bombyx_mori
+    bemisia_tabaci_ssa1ug   
+    bemisia_tabaci_ssa2nig  
+    bemisia_tabaci_ssa3nig   
+    bemisia_tabaci_sweetpotug   
+    biomphalaria_glabrata  
+    bombus_impatiens   
+    bombus_terrestris   
+    bombyx_mori  
     branchiostoma_lanceolatum
-    brugia_malayi
-    caenorhabditis_brenneri
-    caenorhabditis_briggsae
-    caenorhabditis_elegans
-    caenorhabditis_japonica
-    caenorhabditis_remanei
-    capitella_teleta
+    brugia_malayi   
+    caenorhabditis_brenneri   
+    caenorhabditis_briggsae  
+    caenorhabditis_elegans   
+    caenorhabditis_japonica   
+    caenorhabditis_remanei   
+    capitella_teleta   
+    centruroides_sculpturatus_gca000671375v2   
+    ceratitis_capitata_gca000347755v4  
     cimex_lectularius
-    clytia_hemisphaerica_gca902728285
-    crassostrea_gigas
-    culex_quinquefasciatus
-    culex_quinquefasciatus_gca015732765v1
+    clytia_hemisphaerica_gca902728285   
+    copidosoma_floridanum_gca000648655v2  
+    cotesia_glomerata_gca020080835v1   
+    crassostrea_gigas   
+    crassostrea_virginica_gca002022765v4   
+    culex_quinquefasciatus   
+    culex_quinquefasciatus_gca015732765v1  
     culicoides_sonorensis
     danaus_plexippus
-    daphnia_magna
-    daphnia_pulex
-    dendroctonus_ponderosae
+    daphnia_magna   
+    daphnia_pulex   
+    dendroctonus_ponderosae_gca000355655v1   
+    dendronephthya_gigantea_gca004324835v1  
     dermacentor_silvarum_gca013339745v1
-    dinothrombium_tinctorium
-    drosophila_ananassae
-    drosophila_erecta
-    drosophila_grimshawi
-    drosophila_melanogaster
-    drosophila_mojavensis
+    dermatophagoides_pteronyssinus_gca001901225v2   
+    diabrotica_virgifera_gca003013835v2   
+    dinothrombium_tinctorium   
+    diuraphis_noxia_gca001186385v1   
+    drosophila_ananassae   
+    drosophila_erecta  
+    drosophila_grimshawi   
+    drosophila_melanogaster   
+    drosophila_mojavensis  
     drosophila_persimilis
     drosophila_pseudoobscura
     drosophila_sechellia
-    drosophila_simulans
-    drosophila_virilis
+    drosophila_simulans   
+    drosophila_virilis  
     drosophila_willistoni
-    drosophila_yakuba
-    folsomia_candida
+    drosophila_yakuba   
+    folsomia_candida  
+    gigantopelta_aegis_gca016097555v1
     glossina_austeni
     glossina_brevipalpis
-    glossina_fuscipes
-    glossina_morsitans
+    glossina_fuscipes   
+    glossina_morsitans  
     glossina_pallidipes
-    glossina_palpalis
-    haemaphysalis_longicornis_gca013339765v1
-    heliconius_melpomene
-    helobdella_robusta
-    hofstenia_miamia
+    glossina_palpalis   
+    haemaphysalis_longicornis_gca013339765v1  
+    heliconius_melpomene   
+    helobdella_robusta  
+    hermetia_illucens_gca905115235v1   
+    hofstenia_miamia  
     hyalomma_asiaticum_gca013339685v1
-    hymenolepis_microstoma
+    hydra_vulgaris_gca000004095v1   
+    hymenolepis_microstoma  
     ixodes_persulcatus_gca013358835v1
-    ixodes_scapularis
-    ixodes_scapularis_gca016920785v2
-    ixodes_scapularis_ise6
-    lepeophtheirus_salmonis
-    leptotrombidium_deliense
-    lingula_anatina
-    loa_loa
-    lottia_gigantea
+    ixodes_scapularis   
+    ixodes_scapularis_gca016920785v2  
+    leptinotarsa_decemlineata_gca000500325v2   
+    limulus_polyphemus_gca000517525v1   
+    lottia_gigantea  
     lucilia_cuprina
-    lutzomyia_longipalpis
-    mayetiola_destructor
-    megaselia_scalaris
+    lutzomyia_longipalpis   
+    lytechinus_variegatus_gca018143015v1  
+    mayetiola_destructor   
+    megaselia_scalaris  
     melitaea_cinxia
-    mnemiopsis_leidyi
+    metaseiulus_occidentalis_gca000255335v1   
+    mnemiopsis_leidyi   
+    monomorium_pharaonis_gca013373865v2  
     musca_domestica
-    nasonia_vitripennis
-    necator_americanus
-    nematostella_vectensis
-    octopus_bimaculoides
+    nasonia_vitripennis   
+    necator_americanus  
+    nematostella_vectensis   
+    octopus_bimaculoides  
     onchocerca_volvulus
-    orchesella_cincta
+    onthophagus_taurus_gca000648695v2   
+    ooceraea_biroi_gca003672135v1   
+    orbicella_faveolata_gca002042975v1   
+    orchesella_cincta  
+    orussus_abietinus_gca000612105v2   
+    parasteatoda_tepidariorum_gca000365465v3  
     pediculus_humanus
+    penaeus_monodon_gca015228065v1
     phlebotomus_papatasi
-    pristionchus_pacificus
-    rhipicephalus_microplus_gca013339725v1
-    rhipicephalus_sanguineus_gca013339695v1
+    phlebotomus_perniciosus_gca918844115
+    pocillopora_damicornis_gca003704095v1   
+    pomacea_canaliculata_gca003073045v1   
+    priapulus_caudatus_gca000485595v2  
+    pristionchus_pacificus   
+    rhagoletis_pomonella_gca013731165v1   
+    rhipicephalus_microplus_gca013339725v1   
+    rhipicephalus_sanguineus_gca013339695v1  
     rhodnius_prolixus
-    sarcoptes_scabiei
+    rhopalosiphum_maidis_gca003676215v3   
+    saccoglossus_kowalevskii_gca000003605v1   
+    sarcoptes_scabiei  
     schistosoma_mansoni
+    sipha_flava_gca003268045v1
     solenopsis_invicta
-    stegodyphus_mimosarum
-    stomoxys_calcitrans
-    strigamia_maritima
-    strongylocentrotus_purpuratus
+    stegodyphus_mimosarum   
+    stomoxys_calcitrans   
+    strigamia_maritima   
+    strongylocentrotus_purpuratus  
     strongyloides_ratti
+    stylophora_pistillata_gca002571385v1
     teleopsis_dalmanni
-    tetranychus_urticae
-    thelohanellus_kitauei
-    trialeurodes_vaporariorum_gca011764245
-    tribolium_castaneum
-    trichinella_spiralis
-    trichoplax_adhaerens
-    trichuris_muris
-    tigriopus_californicus_gca007210705
-    varroa_destructor_gca002443255
+    tetranychus_urticae   
+    thelohanellus_kitauei   
+    tigriopus_californicus_gca007210705  
+    trialeurodes_vaporariorum_gca011764245   
+    tribolium_castaneum   
+    trichinella_spiralis   
+    trichogramma_pretiosum_gca000599845v3  
+    trichoplax_adhaerens   
+    trichuris_muris   
+    varroa_destructor_gca002443255   
     zootermopsis_nevadensis
   )];
 
